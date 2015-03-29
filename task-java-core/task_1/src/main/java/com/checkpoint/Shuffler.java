60477b04ecac3b5a8aabc293e81f97801b2b86bc
IcedNecro <romanstatkevich@gmail.com>
2015-03-29 16:58:59 +0300

    Task 1 finished

--- task-java-core/task_1/src/main/java/com/checkpoint/Shuffler.java
+++ task-java-core/task_1/src/main/java/com/checkpoint/Shuffler.java
@@ -1,69 +1,78 @@
 package com.checkpoint;
 
+import java.util.Random;
+
 public class Shuffler {
+
+	private static float MAX_SWAP = 0.25f;
+	
 	public static Object[] shuffle(Object arr[]) {
-		int count = (int) (0.25*arr.length);
-		Object[] initialArray = arr.clone();
-		while (true) {
-			int shufflePosition = (int) (arr.length*Math.random());
-			int swapPosition = (int) (arr.length*Math.random());
-			
-			Object buf = arr[shufflePosition];
-			arr[shufflePosition] = arr[swapPosition];
-			arr[swapPosition] = buf;
-			
-			int shuffledElementsCount = getShuffledElementsCount(initialArray, arr);
-			
-			if(shuffledElementsCount>count) {
-				try {
-					Object[][] o = (Object[][])arr;
-					o.clone();
-					
-					for(int i=0; i<((Object[][])arr).length; i++) {
-						System.out.println("before");
-						outputArray((Object[]) arr[i]);
-						arr[i] = shuffle((Object[])arr[i]);
-						System.out.println("after");
-						outputArray((Object[]) arr[i]);
-					}
-					return arr;
-				} catch(Exception e) {
-					return arr;
-				}
-			}
+		try {
+			Object[][] buffer = (Object[][]) arr;
+			return (Object[]) doubleDimensional(buffer);
+		}catch(Exception e) {
+			return oneDimensional(arr);
 		}
 	}
-
-	public static void anotherShuffle(Object[] objects) {
-		int depth=1;
-		Object[] arr = objects;
-		do{
-			try{
-				arr = (Object[][]) arr;
-			}catch(Exception e){
-				break;
-			}
-			depth++;
-		}while(true);
-		System.out.println(depth + " depth");
+	
+	private static Object[][] doubleDimensional(Object[][] arr) {
+		Object[][] buffer = arr.clone();
+		for(int i = 0; i<arr.length; i++) {
+			buffer[i] = arr[i].clone();
+		}
+		
+		int countToSwap = (int) (arr.length*arr[0].length*MAX_SWAP);
+		
+		Random random = new Random();
+		
+		while(countToSwap>=checkForIdentity(arr, buffer)) {
+			int swapFromX = random.nextInt(arr.length);
+			int swapFromY = random.nextInt(arr.length);
+			
+			int swapToX = random.nextInt(arr.length);
+			int swapToY = random.nextInt(arr.length);
+			
+			Object buf = arr[swapFromX][swapFromY];
+			arr[swapFromX][swapFromY] = arr[swapToX][swapToY];
+			arr[swapToX][swapToY] = buf;
+		}
+		
+		return arr;
 	}
 	
-	public static void outputArray(Object[] o) {
-		for (int i = 0; i < o.length; i++)
-			System.out.print(o[i] + " ");
-		System.out.println();
+	private static Object[] oneDimensional(Object[] arr) {
+		int countToSwap = (int) (arr.length*MAX_SWAP);
+		Object[] buffer = arr.clone();
+		Random random = new Random();
+		
+		while(countToSwap>=checkForIdentity(arr, buffer)) {
+			int swapFrom = random.nextInt(arr.length);
+			int swapTo = random.nextInt(arr.length);
+			
+			Object buf = arr[swapFrom];
+			arr[swapFrom] = arr[swapTo];
+			arr[swapTo] = buf;
+		}
+			
+		return arr;
 	}
-
 	
-	public static int getShuffledElementsCount(
-			Object[] initialArray,
-			Object[] currentArray) {
-		int count = 0;
-		
-		for(int i=0; i<initialArray.length; i++)
-			if(!initialArray[i].equals(currentArray[i]))
-				count++;
-		
-		return count;
+	public static Integer checkForIdentity(Object[] current, Object[] initial) {
+		int numberOfNotEqual = 0;
+		try
+		{
+			Object[][] buffer = (Object[][]) current;
+			Object[][] initialBuffer = (Object[][]) initial;
+			
+			for(int i=0; i<buffer.length; i++)
+				for(int j=0; j<buffer[i].length; j++)
+					if(buffer[i][j]!=initialBuffer[i][j])
+						numberOfNotEqual++;
+		} catch(Exception e){
+			for(int i=0; i<current.length; i++)
+				if(current[i]!=initial[i])
+					numberOfNotEqual++;
+		}
+		return numberOfNotEqual;
 	}
 }
